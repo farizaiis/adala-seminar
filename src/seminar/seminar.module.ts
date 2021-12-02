d3fbdddd1b7960d7b2c172b1e38565d9f5aac1e2
farizaiis <farizaiis@hotmail.com>
2021-12-03 02:12:44 +0700

    disable prettier for seminarmodule

--- src/seminar/seminar.module.ts
+++ src/seminar/seminar.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { SeminarService } from './service/seminar.service';
 import { SeminarController } from './controller/seminar.controller';
