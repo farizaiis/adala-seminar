ed6f52edb67167fe7fe33ee50f3862d530da89ab
farizaiis <farizaiis@hotmail.com>
2021-12-06 05:56:58 +0700

    set authorization user is user

--- src/seminar/controller/seminar.controller.ts
+++ src/seminar/controller/seminar.controller.ts
@@ -10,9 +10,11 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { catchError, Observable, of, map } from 'rxjs';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-guards';
 import { Seminar } from '../models/seminar.interface';
 import { SeminarService } from '../service/seminar.service';
 
@@ -21,6 +23,7 @@ export class SeminarController {
   constructor(private seminarService: SeminarService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   // eslint-disable-next-line @typescript-eslint/ban-types
   create(@Body() seminar: Seminar): Observable<Seminar | Object> {
     return this.seminarService.create(seminar).pipe(
@@ -30,11 +33,13 @@ export class SeminarController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param() params): Observable<Seminar> {
     return this.seminarService.findOne(params.id);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   index(
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 15
@@ -48,11 +53,13 @@ export class SeminarController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   deleteOne(@Param('id') id: string): Observable<Seminar> {
     return this.seminarService.deleteOne(Number(id));
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   updateOne(
     @Param('id') id: string,
     @Body() seminar: Seminar
