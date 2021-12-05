c8c20ea97ddbc33f20fbc1263480fba77bc0c9bb
farizaiis <farizaiis@hotmail.com>
2021-12-06 05:37:08 +0700

    set pagination

--- src/seminar/controller/seminar.controller.ts
+++ src/seminar/controller/seminar.controller.ts
@@ -1,20 +1,24 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+/* eslint-disable prettier/prettier */
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Post,
-    Put,
-  } from '@nestjs/common';
-  import { catchError, Observable, of, map } from 'rxjs';
-  import { Seminar } from '../models/seminar.interface';
-  import { SeminarService } from '../service/seminar.service';
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { catchError, Observable, of, map } from 'rxjs';
+import { Seminar } from '../models/seminar.interface';
+import { SeminarService } from '../service/seminar.service';
 
 @Controller('seminar')
 export class SeminarController {
-    constructor(private seminarService: SeminarService) {}
+  constructor(private seminarService: SeminarService) {}
 
   @Post()
   // eslint-disable-next-line @typescript-eslint/ban-types
@@ -31,8 +35,16 @@ export class SeminarController {
   }
 
   @Get()
-  findAll(): Observable<Seminar[]> {
-    return this.seminarService.findAll();
+  index(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 15
+  ): Observable<Pagination<Seminar>> {
+    limit = limit > 15 ? 15 : limit;
+    return this.seminarService.paginate({
+      page: Number(page),
+      limit: Number(limit),
+      route: 'http://localhost:3000/seminar',
+    });
   }
 
   @Delete(':id')
@@ -41,7 +53,10 @@ export class SeminarController {
   }
 
   @Put(':id')
-  updateOne(@Param('id') id: string, @Body() seminar: Seminar): Observable<Seminar> {
+  updateOne(
+    @Param('id') id: string,
+    @Body() seminar: Seminar
+  ): Observable<Seminar> {
     return this.seminarService.updateOne(Number(id), seminar);
   }
 }
