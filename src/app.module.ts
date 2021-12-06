e0ef320a244708941839857b09632c016b034296
farizaiis <farizaiis@hotmail.com>
2021-12-06 07:20:14 +0700

    fix bug at user and seminar entity and app module for relation database

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { SeminarModule } from './seminar/seminar.module';
+import { ParticipantModule } from './participant/participant.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { SeminarModule } from './seminar/seminar.module';
     UserModule,
     AuthModule,
     SeminarModule,
+    ParticipantModule,
   ],
   controllers: [AppController],
   providers: [AppService],
